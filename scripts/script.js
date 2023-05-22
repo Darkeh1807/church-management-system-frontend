29ab7c4472f870b3a86474adcfca531177f4e163
Elijah Darkeh <elijahagbedam18@gmail.com>
2023-05-22 23:05:28 +0000

    first release

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+function hideSignIn() {
+    document.getElementById("loginForm").style.display = "none";
+    document.getElementById("registerForm").style.display = "";
+  }
+
+  function hideRegister() {
+    document.getElementById("registerForm").style.display = "none";
+    document.getElementById("loginForm").style.display = "";
+  }
